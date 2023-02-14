b919f8a83a1ef9ed1af4ea790ff1fbc3c7d74de5
theanuragshukla <tachyonanurag@gmail.com>
2023-02-14 16:38:14 +0530

    update

---
+++ app/src/main/java/xyz/anuragshukla/java/chat/Constants.java
@@ -0,0 +1,7 @@
+package xyz.anuragshukla.java.chat;
+
+public class Constants {
+    private static String localS = "http://10.0.2.2:3000";
+    private static String remoteS = "https://droidchatserver.onrender.com";
+    public static String server = remoteS;
+}
